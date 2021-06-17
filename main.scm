739240d3df8ebb0699f895d7599a80f4c8259d75
Lorena Sanchez <40070352+lqrena@users.noreply.github.com>
2021-06-17 23:39:57 +0000

    n2 final

--- main.scm
+++ main.scm
@@ -1,11 +1,11 @@
-(define (somarTudo um dez vinte cinq real)
-  (+ (* um 1) (* dez 10) (* vinte 20) (* cinq 50) (* real 100)) 
+(define (somarTudo um cinco dez vinte cinq real)
+  (+ (* um 1) (* cinco 1) (* dez 10) (* vinte 20) (* cinq 50) (* real 100)) 
 )
 
 (define (calcularReais totalCentavos)
   (print (/ totalCentavos 100 ) " reais ou "totalCentavos " centavos")
 )
 
-(define (supermercado um dez vinte cinq real)
-  (calcularReais(somarTudo um dez vinte cinq real))
+(define (supermercado um cinco dez vinte cinq real)
+  (calcularReais(somarTudo um cinco dez vinte cinq real))
 )
